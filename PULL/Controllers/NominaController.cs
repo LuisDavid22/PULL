ddb9e903e29f107fb9f4d03db02a1a43a22869c2
Luis laureano <laureano.luis.11@gmail.com>
2019-06-07 21:56:50 -0400

    agregando archivos

---
+++ PULL/Controllers/NominaController.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PULL.Controllers
+{
+    public class NominaController : Controller
+    {
+        tssDBEntities db = new tssDBEntities();
+        // GET: Nomina
+        public async Task<ActionResult> Index()
+        {
+            var httpClient = new HttpClient();
+            var json = await httpClient.GetStringAsync("http://localhost:53231/empleado");
+
+            ClaseEsquema clase = JsonConvert.DeserializeObject<ClaseEsquema>(json);
+
+
+
+            Nomina cabecera = new Nomina
+            {
+                Periodo = Convert.ToDateTime(clase.Periodo),
+                Rnc = clase.Rnc,
+                Nomina_Detalle = clase.Empleados
+
+            };
+
+            db.Nomina.Add(cabecera);
+            db.SaveChanges();
+
+
+            return Content("Datos guardados correctamente");
+        }
+    }
+    class ClaseEsquema
+    {
+        public int Id { get; set; }
+        public string Rnc { get; set; }
+        public string Periodo { get; set; }
+        public int CantEmpleados { get; set; }
+        public ICollection<Nomina_Detalle> Empleados { get; set; }
+
+
+    }
+}
